54ccfe2f63e138a3e3b8f76b0be20fe3edd2a25e
Mateusz <kapuscinski.mateusz01@gmail.com>
2025-05-08 14:24:21 +0200

    play sound on duration state update

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,19 +8,22 @@ function Calculator({ workouts, allowSound }) {
   const [durationBreak, setDurationBreak] = useState(5);
   const [duration, setDuration] = useState(0);
 
-  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
-
-  const playSound = function () {
-    if (!allowSound) return;
-    const sound = new Audio(clickSound);
-    sound.play();
-  };
 
   useEffect(() => {
     setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
   }, [number, sets, speed, durationBreak]);
+
+  useEffect(() => {
+    function playSound() {
+      if (!allowSound) return;
+      const sound = new Audio(clickSound);
+      sound.play();
+    }
+
+    playSound();
+  }, [allowSound, duration]);
 
   function handleInc() {
     setDuration((duration) => Math.floor(duration) + 1);
